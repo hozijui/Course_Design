57586339dd6bf2f6e620eebf13addd71b2ded838
Ho_zijui <hozijui@gmail.com>
2018-12-09 15:57:04 +0800

    Linux下按键控制LED

---
+++ Stage1/led.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <errno.h>
+
+int main()
+{
+  int i;
+  int led_fd,button_fd;
+  int leds[4]={0,0,0,0};
+
+    led_fd=open("/dev/leds",0);
+    button_fd=open("/dev/buttons",0);
+
+    for(i=0;i<4;i++)
+      ioctl(led_fd,leds[i],i);
+
+    while(1){
+      char pressed[6];
+      read(button_fd,pressed,sizeof pressed);
+      for(i=0;i<4;i++){
+        leds[i]=(leds[i]+pressed[i])%2;
+        ioctl(led_fd,leds[i],i);
+	    }
+    }      
+
+    close(led_fd);
+    close(button_fd);
+  return 0;
+}
